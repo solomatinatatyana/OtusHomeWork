4ff715f7b2b3fb1160f79a15aad7307437dcf325
tsolomatina <Hfgbl123>
2020-01-28 16:53:14 +0300

    upd log

--- src/main/java/config/BaseWebDrivingTest.java
+++ src/main/java/config/BaseWebDrivingTest.java
@@ -27,8 +27,8 @@ public class BaseWebDrivingTest extends BaseTest {
     @BeforeClass(alwaysRun = true)
     public void setUp(){
         super.setUp();
-        log.info("Test: [" + this.getClass().asSubclass(this.getClass()).getSimpleName() + "]");
-        log.info("Browser: [" + BrowserType.valueOf(browser) + "]");
+        log.info("Test: [{}]",this.getClass().asSubclass(this.getClass()).getSimpleName());
+        log.info("Browser: [{}]",BrowserType.valueOf(browser));
         testData = ConfigFactory.create(TestDataConfig.class);
         this.options = new MutableCapabilities();
         this.webApp = new WebApplicationManager(BrowserType.valueOf(browser), options);
