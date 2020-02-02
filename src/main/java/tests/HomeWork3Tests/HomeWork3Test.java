befccb08bc7a98c7de1d5535e8d3ffa1ba5dcc88
Tatiana <tokio9507@mail.ru>
2020-02-03 00:18:59 +0300

    Добавлен тест на добавление к сравнению и исправлен соотв метод. Часть 2 - отладить с dataProvider. Без него работает нормально

--- src/main/java/tests/HomeWork3Tests/HomeWork3Test.java
+++ src/main/java/tests/HomeWork3Tests/HomeWork3Test.java
@@ -4,12 +4,13 @@ import config.BaseWebDrivingTest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,10 +19,26 @@ import java.util.List;
 //@Test(groups = "smoke")
 public class HomeWork3Test extends BaseWebDrivingTest {
     private Logger log = LogManager.getLogger(HomeWork3Test.class);
+    private SoftAssert softAssert = new SoftAssert();
 
-    //======Тестовые данные====
+    //=======================Тестовые данные=================
     private List<String> testSmartPhones = new ArrayList<>();
     private static final String baseUrl = "https://market.yandex.ru/";
+    private static String nameOfGood;
+    private enum ModelSmartPhones{
+        HUAWEI("HUAWEI"),
+        XIAOMI("Xiaomi");
+        private String model;
+        ModelSmartPhones(String model) { this.model = model; }
+        public String getModel() { return model; }
+    }
+
+    @DataProvider(name = "models")
+    public Object[][] Browsers() {
+        return new Object[][] {
+                {ModelSmartPhones.HUAWEI.getModel()},
+                {ModelSmartPhones.XIAOMI.getModel()}
+        };}
 
     @BeforeClass(alwaysRun = true)
     public void init(){
@@ -32,23 +49,21 @@ public class HomeWork3Test extends BaseWebDrivingTest {
         this.sortByPrice();
     }
 
-    @Test(description = "Проверить, что отобразилась плашка 'Товар {HUAWEI} добавлен к сравнению'")
-    public void testHuawei(){
-        log.info("Проверка добавления HUAWEI к сравнению");
-        this.addSmartPhoneToCompare("HUAWEI");
-        //Проверить, что отобразилась плашка "Товар {RedMi} добавлен к сравнению"
-    }
-
-    @Test(description = "Проверить, что отобразилась плашка 'Товар {Xiaomi} добавлен к сравнению'",
-    dependsOnMethods = "testHuawei")
-    public void testXiaomi(){
-        log.info("Проверка добавления Xiaomi к сравнению");
-        this.addSmartPhoneToCompare("Xiaomi");
-        //Проверить, что отобразилась плашка "Товар {Xiaomi} добавлен к сравнению"
+    @Test(description = "Проверить, что отобразилась плашка 'Товар {товар} добавлен к сравнению'",
+            dataProvider = "models")
+    public void testGoodToCompare(String model) throws InterruptedException {
+        log.info("Проверка добавления "+ model +" к сравнению");
+        addSmartPhoneToCompare(model);
+        WebElement paneNotify = (new WebDriverWait(driver, 30))
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.popup-informer__title")));
+        String textLabel = paneNotify.getText();
+        softAssert.assertTrue(paneNotify.isDisplayed(),"плашка [Товар {"+ nameOfGood +"} добавлен к сравнению] не отображается");
+        softAssert.assertEquals(textLabel,"Товар "+ nameOfGood +" добавлен к сравнению", "Текст сообщения неверный");
+        softAssert.assertAll();
     }
 
     /*@Test(description = "Перейти в раздел Сравнение. Проверить, что в списке товаров 2 позиции",
-            dependsOnMethods = "testXiaomi")
+            dependsOnMethods = "testGoodToCompare")
     public void checkComparingGoods(){
         //todo
         log.info("Проверка Раздела Сравнение");
@@ -71,7 +86,6 @@ public class HomeWork3Test extends BaseWebDrivingTest {
     }*/
 //-------------------------------------------------METHODS--------------------------------------------------------------
     private void filterSmartPhones(List<String> smartPhones){
-        WebDriverWait wait = new WebDriverWait(driver,10L);wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//fieldset[@data-autotest-id='7893318']")));
         WebElement fieldsetBrand = driver.findElement(By.xpath(".//fieldset[@data-autotest-id='7893318']"));
         smartPhones.forEach(e->{
             fieldsetBrand.findElement(By.xpath(".//div/span[contains(text(),'"+ e +"')]")).click();
@@ -87,31 +101,13 @@ public class HomeWork3Test extends BaseWebDrivingTest {
         log.info("Список телефонов отсортирован");
     }
 
-    public boolean retryingFindClick(By by) {
-        boolean result = false;
-        int attempts = 0;
-        while(attempts < 2) {
-            try {
-                driver.findElement(by).click();
-                result = true;
-                break;
-            } catch(StaleElementReferenceException e) {
-            }
-            attempts++;
-        }
-        return result;
-    }
-
-    private void addSmartPhoneToCompare(String model){
-        //todo исправить ожидания и улучить локаторы, избавиться от StaleException
-        WebDriverWait wait = new WebDriverWait(driver,10L);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".n-snippet-list")));
-        driver.findElements(By.cssSelector(".n-snippet-list>div.n-snippet-cell2"));
-        List<WebElement> good1 = driver.findElements(By.cssSelector(".n-snippet-list>div.n-snippet-cell2 a[title*="+ model +"]"));
-        WebElement parent = good1.get(0).findElement(By.xpath(".."));
-        parent.findElement(By.cssSelector("div.n-product-toolbar__item")).click();
-        log.info(parent.getText());
-        log.info("Телефон [{}] добавлен к сравнению", model);
+    private void addSmartPhoneToCompare(String model) throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver,50L);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(".//div[contains(@class,'preloadable__preloader')]")));
+        WebElement good = driver.findElement(By.xpath(".//a[contains(@title,'"+ model +"')]/.."));
+        good.findElement(By.cssSelector("div.n-product-toolbar__item")).click();
+        nameOfGood = good.findElement(By.cssSelector("a[title*="+ model +"]")).getAttribute("title");
+        log.info("Телефон [{}] добавлен к сравнению", nameOfGood);
     }
 
 }
