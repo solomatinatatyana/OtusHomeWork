5e1ca42047178a37b5716ff08975f69b6947ad9e
Tatiana <tokio9507@mail.ru>
2020-03-01 23:49:02 +0300

    HomeWork5Test - исправлен и доработан

--- src/main/java/pages/otusPages/ProfilePages/PersonalDataPage.java
+++ src/main/java/pages/otusPages/ProfilePages/PersonalDataPage.java
@@ -1,7 +1,6 @@
 package pages.otusPages.ProfilePages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
